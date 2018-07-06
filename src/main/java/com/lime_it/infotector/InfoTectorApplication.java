f3f1637351de9d50719c9bc3d33281b4e682f935
seojeonghyeon <seojeonghyeon0630@gmail.com>
2018-07-06 10:47:05 +0900

    Project Setting Finish

---
+++ src/main/java/com/lime_it/infotector/InfoTectorApplication.java
@@ -0,0 +1,12 @@
+package com.lime_it.infotector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoTectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoTectorApplication.class, args);
+	}
+}
